2cdb5d979a896486035152d64be81337c86902b4
gowthamkishoreindukuri <50141541+gowthamkishoreindukuri@users.noreply.github.com>
2020-06-19 23:00:44 +0530

    new

---
+++ set-1/queues/p3.cpp
@@ -0,0 +1,10 @@
+//Show how you can efficiently implement one stack using two queues. Analyze the
+//running time of the stack operations.
+#include<bits/stdc++.h>
+using namespace std;
+queue<int> q1,q2;
+
+int main()
+{
+
+}
